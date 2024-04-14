10cc6d3456a85bf634acdb4d2507e59c6fc44b8a
Ilan Cukierman <ilancukierman@Ilans-MacBook-Air.local>
2024-04-14 16:39:03 -0300

    setup database connection

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
+
+mongoose
+  .connect(process.env.MONGO_URI, {})
+  .then(() => console.log("MongoDB conectado"))
+  .catch((err) => console.log(err));
 
 module.exports = app;
