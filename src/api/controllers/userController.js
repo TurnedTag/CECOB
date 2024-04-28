de6d731dd5e9c8774774418943db3e6c0f1977cb
Ilan Cukierman <ilancukierman@Ilans-MacBook-Air.local>
2024-04-28 16:41:06 -0300

    create login method

--- src/api/controllers/userController.js
+++ src/api/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   const { username, email, password } = req.body;
@@ -24,4 +25,29 @@ const register = async (req, res) => {
   }
 };
 
-module.exports = { register };
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).send("Por favor, informe um email e uma senha");
+  }
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user || !(await user.checkPassword(password))) {
+      return res.status(401).send("Autenticação falhou");
+    }
+    const token = jwt.sign({ id: user._id }, process.env.SECRET, {
+      expiresIn: "1d",
+    });
+    res.status(200).send({ user, token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+};
+
+module.exports = {
+  register,
+  login,
+};
