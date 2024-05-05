051598a3376f1483adb36072dcb04bb3ad97399a
Ilan Cukierman <ilancukierman@Ilans-MacBook-Air.local>
2024-05-05 18:03:03 -0300

    create listActivity

--- src/api/controllers/activityController.js
+++ src/api/controllers/activityController.js
@@ -1,14 +1,14 @@
 const Activity = require("../models/Activity");
 
 const createActivity = async (req, res) => {
-  const { description, address, title } = req.body;
-
-  if (!description) {
-    return res.status(400).send({ message: "Description is required" });
-  }
+  const { title, description, address } = req.body;
 
   if (!title) {
     return res.status(400).send({ message: "Title is required" });
+  }
+
+  if (!description) {
+    return res.status(400).send({ message: "Description is required" });
   }
 
   if (!address) {
@@ -30,15 +30,23 @@ const createActivity = async (req, res) => {
       author: req.user._id,
       description,
       address,
-      title,
     });
     res.status(200).json(newActivity);
   } catch (error) {
-    console.log(error);
     res.status(500).send({ message: error.message || "Internal server error" });
+  }
+};
+
+const listActivities = async (req, res) => {
+  try {
+    const activities = await Activity.find().populate("author", "username");
+    res.status(200).json(activities);
+  } catch (error) {
+    res.status(500).json({ message: error.message || "Internal server error" });
   }
 };
 
 module.exports = {
   createActivity,
+  listActivities,
 };
--- src/api/routes/activityRoutes.js
+++ src/api/routes/activityRoutes.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 const protect = require("../middlewares/authMiddleware");
 
-const { createActivity } = require("../controllers/activityController");
+const {
+  createActivity,
+  listActivities,
+} = require("../controllers/activityController");
 
 router.post("/activities", protect, createActivity);
+router.get("/activities", protect, listActivities);
 
 module.exports = router;
