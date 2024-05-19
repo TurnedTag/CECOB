ccd9f2335de1654c3d09daef482202a47e54789a
Ilan Cukierman <ilan.cukierman@gmail.com>
2024-05-19 18:39:55 -0300

    coloquei o cep

--- src/api/models/User.js
+++ src/api/models/User.js
@@ -5,6 +5,7 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  cep: { type: String, required: true },
 });
 
 UserSchema.pre("save", async function (next) {
